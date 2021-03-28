f4430c518a6e3ccd21cc9811069ab0f50694b043
Joshua Decosta <josh@Joshuas-M1.lan>
2021-03-28 15:33:36 -0400

    adds basica functionality to scrapers

--- getGeocode.js
+++ getGeocode.js
@@ -2,39 +2,50 @@ const dotenv = require("dotenv");
 //note: this only works locally; in Lambda we use environment variables set manually
 dotenv.config();
 
-const { Client } = require("@googlemaps/google-maps-services-js");
+// const { Client } = require("@googlemaps/google-maps-services-js");
+const { getLatLngPelias } = require("./lib/distance-utils");
 const { generateKey } = require("./data/dataDefaulter");
 const axios = require("axios");
 const axiosRetry = require("axios-retry");
 
 axiosRetry(axios, { retries: 5, retryDelay: axiosRetry.exponentialDelay });
 
-const getGeocode = async (name, street, zip) => {
-    const address = `${name},MA,${street},${zip}`;
-    const client = new Client();
+const formatAddress = (name, street, city, zip) => {
+    var result = ""
+    if (name != "") {
+        result += `${name},`
+    }
+    if (street != "") {
+        result += `${street},`
+    }
+    if (city != "") {
+        result += `${city},`
+    }
+    result += "Ma,"
+    if (zip != "") {
+        result += `${zip}`
+    }
+    return result
+
+}
+
+const getGeocode = async (name, street, city, zip) => {
+    const address = formatAddress(name, street, city, zip);
     try {
-        const resp = await client.geocode(
-            {
-                params: {
-                    address,
-                    key: process.env.GOOGLE_API_KEY,
-                },
-            },
-            axios
-        );
-        return resp.data;
+        return await getLatLngPelias(address)
     } catch (e) {
-        console.error(e.response.data);
+        console.error(e);
     }
 };
 
 const getAllCoordinates = async (locations, cachedResults) => {
     const existingLocations = cachedResults.reduce((acc, location) => {
-        const { latitude, longitude } = location;
+        const { latitude, longitude, street } = location;
         if (latitude && longitude) {
             acc[generateKey(location)] = {
                 latitude,
                 longitude,
+                street
             };
             return acc;
         } else {
@@ -44,24 +55,24 @@ const getAllCoordinates = async (locations, cachedResults) => {
 
     const coordinateData = await Promise.all(
         locations.map(async (location) => {
-            const { name = "", street = "", zip = "" } = location;
+            const { name = "", street = "", city= "", zip = "" } = location;
             const locationInd = generateKey(location);
 
-            if (existingLocations[locationInd]) {
-                return { ...location, ...existingLocations[locationInd] };
-            } else {
-                const locationData = await getGeocode(name, street, zip);
+            // if (existingLocations[locationInd]) {
+            //     return { ...location, ...existingLocations[locationInd] };
+            // } else {
+                const locationData = await getGeocode(name, street,city, zip);
 
                 if (locationData) {
                     return {
                         ...location,
                         latitude:
-                            locationData?.results[0].geometry.location.lat,
+                            locationData?.lat,
                         longitude:
-                            locationData?.results[0].geometry.location.lng,
+                            locationData?.lng,
                     };
                 } else return location;
-            }
+            // }
         })
     );
     return coordinateData;
